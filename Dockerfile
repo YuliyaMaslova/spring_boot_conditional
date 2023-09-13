493705142f8078afc236d71d2618964e7705bf94
Yuliya Maslova <uu.naumova@gmail.com>
2023-09-13 21:10:23 +0300

    Интеграционное тестирование

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM openjdk:17-jdk-alpine
+
+EXPOSE 8081
+
+ADD target/SpringBootDemo-0.0.1-SNAPSHOT.jar myapp.jar
+
+
+ENTRYPOINT ["java","-jar","/myapp.jar"]
--- src/test/java/ru/netology/springbootdemo/SpringBootDemoApplicationTests.java
+++ src/test/java/ru/netology/springbootdemo/SpringBootDemoApplicationTests.java
@@ -1,13 +1,40 @@
 package ru.netology.springbootdemo;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootDemoApplicationTests {
 
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    private static final GenericContainer<?> myDevApp = new GenericContainer<>("devapp")
+            .withExposedPorts(8080);
+    private static final GenericContainer<?> myProdApp = new GenericContainer<>("prodapp")
+            .withExposedPorts(8081);
+    @BeforeAll
+    static void setUp() {
+        myDevApp.start();
+        myProdApp.start();
+    }
+
     @Test
-    void contextLoads() {
+    void devProfileTest() {
+        ResponseEntity<String> forEntity = restTemplate.getForEntity("http://localhost:" + myDevApp.getMappedPort(8080) + "/profile", String.class);
+        Assertions.assertEquals("Current profile is dev", forEntity.getBody());
+    }
+
+    @Test
+    void prodProfileTest() {
+        ResponseEntity<String> forEntity = restTemplate.getForEntity("http://localhost:" + myProdApp.getMappedPort(8081) + "/profile", String.class);
+        Assertions.assertEquals("Current profile is production", forEntity.getBody());
     }
 
 }
